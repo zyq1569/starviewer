60fcbc5d84d128391768f2a856415e55a84aa59a
zyq1569 <zyq1569@126.com>
2020-08-08 21:49:24 +0800

    modify  log

--- PacsViewer/src/main/main.cpp
+++ PacsViewer/src/main/main.cpp
@@ -250,7 +250,6 @@ int main(int argc, char *argv[])
     }
     else
     {
-        INFO_LOG("start to creat QApplicationMainWindow");
         udg::QApplicationMainWindow *mainWin = new udg::QApplicationMainWindow; // Main instance, no more running
         // We connect to receive arguments from other instances
         QObject::connect(&app, SIGNAL(messageReceived(QString)), StarviewerSingleApplicationCommandLineSingleton::instance(), SLOT(parseAndRun(QString)));
