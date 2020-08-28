4b9f5ebc4bb734dd32ea12289b4d3c280a188228
zyq1569 <zyq1569@126.com>
2020-08-28 12:49:10 +0800

    modify  comments

--- PacsViewer/src/interface/extensionhandler.h
+++ PacsViewer/src/interface/extensionhandler.h
@@ -29,59 +29,60 @@ namespace udg {
 class QApplicationMainWindow;
 
 /**
-    Gestor de mini-aplicacions i serveis de l'aplicació principal
+   Manager of mini-applications and services of the main application
   */
 class ExtensionHandler : public QObject {
-Q_OBJECT
+    Q_OBJECT
 public:
     ExtensionHandler(QApplicationMainWindow *mainApp, QObject *parent = 0);
 
     ~ExtensionHandler();
 
 public slots:
-    /// Rep la petició d'un servei/mini-aplicació i fa el que calgui
+    /// Receive the request for a service / mini-application and do what is necessary
     void request(int who);
     bool request(const QString &who);
 
-    /// Obrirà l'extensió per defecte. Si no hi ha dades de pacient vàlides, no farà res.
+    /// It will open the default extension. If there is no valid patient data, it will do nothing.
     void openDefaultExtension();
 
-    /// Assigna el contexte de l'extensió
-    /// @param context contexte
+    /// Assigns the context of the extension
+    /// @param context context
     void setContext(const ExtensionContext &context);
 
-    /// Obtenim el contexte de l'extensió
-    /// @return El contexte de l'extensió, es pot modificar
+    /// We get the context of the extension
+    /// @return The context of the extension can be modified
     ExtensionContext& getContext();
 
 private slots:
-    /// Processa un conjunt d'arxius d'input i els processa per decidir què fer amb aquests, com per exemple
-    /// crear nous pacient, obrir finestres, afegir les dades al pacient actual, etc
-    /// @param inputFiles Els arxius a processar, que poden ser del tipus suportat per l'aplicació o no
+    /// Processes a set of input files and processes them to decide what to do with them, such as
+    /// create new patient, open windows, add data to current patient, etc
+    /// @param inputFiles Files to process, which may or may not be supported by the application
     void processInput(const QStringList &inputFiles);
 
-    /// Donada una llista de pacients d'entrada, s'encarrega de posar a punt
-    /// aquests i assignar-lis la finestra adequada, decidint si obrir noves finestres
-    /// i/o fusionar o matxacar el pacient actual d'aquesta finestra.
-    /// La llista s'assumeix que no té pacients iguals separats, és a dir,
-    /// s'assumeix que cada entrada correspon a un pacient diferent, amb un o varis estudis
-    /// Afegim un segon paràmetre que ens indica si els pacients a processar només cal carregar-los o fer-ne un "view"
+    /// Given a list of inpatients, he is in charge of setting up
+    /// these and assign them the appropriate window, deciding whether to open new windows
+    /// and / or merge or mash the current patient in this window.
+    /// The list is assumed to have no separate equal patients, i.e.
+    /// it is assumed that each entry corresponds to a different patient, with one or more studies
+    /// We add a second parameter that tells us if patients just need to be loaded or "viewed".
     void processInput(QList<Patient*> patientsList, bool loadOnly = false);
 
-    /// Es cridarà quan la QueryScreen es tanqui
+    ///It will be called when the QueryScreen closes
     void queryScreenIsClosed();
 
 private:
-    /// Crea les connexions de signals i slots
+    ///Creates signal and slot connections
     void createConnections();
 
-    /// Afegeix un pacient a una mainwindow tenint en compte si cal fusionar o no i si es pot reemplaçar el pacient actual ja carregat
-    /// Afegim un segon paràmetre que ens indica si els pacients a processar només cal carregar-los o fer-ne un "view"
-    /// Retorna la mainwindow a on s'ha afegit el pacient.
+    /// Add a patient to a mainwindow considering whether to merge or
+    /// not and whether to replace the currently loaded current patient
+    /// We add a second parameter that tells us if patients just need to be loaded or "viewed".
+    /// Returns the mainwindow to which the patient was added.
     QApplicationMainWindow* addPatientToWindow(Patient *patient, bool canReplaceActualPatient, bool loadOnly = false);
 
-    /// Processa el pacient donat per tal que pugui ser usat per les extensions
-    //  Li crea els volums al repositori i assigna quina és la sèrie per defecte
+    /// Process the given patient so that it can be used for extensions
+    // Creates the volumes in the repository and assigns the default string
     void generatePatientVolumes(Patient *patient, const QString &defaultSeriesUID);
 
     /// Scans the list and returns a list where the patients which are the same are merged.
@@ -89,19 +90,19 @@ private:
     QList<Patient*> mergePatients(const QList<Patient*> &patientList);
 
 private:
-    /// Punter a l'aplicació principal
+    /// Pointer to the main application
     QApplicationMainWindow *m_mainApp;
 
-    /// Importar models del sistema de fitxers al repositori de volums
+    /// Import file system models into the volume repository
     AppImportFile m_importFileApp;
 
-    /// Contexte de l'extensió
+    ///Context of the extension
     ExtensionContext m_extensionContext;
 
-    /// Indica si a aquesta finestra li pertoca o no tancar la QueryScreen
+    /// Whether or not to close the QueryScreen for this window
     bool m_haveToCloseQueryScreen;
 
-    /// Mutex to avoid concurrent access to the patient comparer singleton.
+    ///Mutex to avoid concurrent access to the patient comparer singleton.
     QMutex m_patientComparerMutex;
 
 };
