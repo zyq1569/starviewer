f1147b051e24963c56e01c5441a7974640810dea
zyq1569 <zyq1569@126.com>
2020-08-31 22:06:44 +0800

    modify  comments

--- PacsViewer/src/inputoutput/qpopuprisrequestsscreen.cpp
+++ PacsViewer/src/inputoutput/qpopuprisrequestsscreen.cpp
@@ -25,15 +25,16 @@
 namespace udg {
 
 QPopUpRISRequestsScreen::QPopUpRISRequestsScreen(QWidget *parent)
- : QNotificationPopup(parent)
+    : QNotificationPopup(parent)
 {
     setOngoingOperationText(tr("%1 will proceed to retrieve it.").arg(ApplicationNameString));
 }
 
 void QPopUpRISRequestsScreen::queryStudiesByAccessionNumberStarted()
 {
-    // Si arriba una altra petició mentre hi ha activat el timer per amagar el PopUp o s'està amagant, hem de fer que aquest no s'amagui per
-    // mostrar la nova petició
+    // If another request arrives while the Timer to hide the PopUp has been activated or is being hidden,
+    // we must make it not hide by
+    // show the new request
     cancelTriggeredAnimations();
 
     setHeadline(tr("%1 has received a request from RIS to retrieve studies.").arg(ApplicationNameString));
@@ -70,13 +71,13 @@ void QPopUpRISRequestsScreen::retrieveDICOMFilesFromPACSJobFinished(PACSJobPoint
 
     if (retrieveDICOMFilesFromPACSJob.isNull())
     {
-        ERROR_LOG("El PACSJob que ha finalitzat no és un RetrieveDICOMFilesFromPACSJob");
+        ERROR_LOG("The completed PACSJob is not a RetrieveDICOMFilesFromPACSJob");
     }
     else
     {
-        // Si quan s'ha descarregat l'estudi el PopUp encara no s'ha mogut a baix a l'esquerre, el que fem es forçar-los a moures sense espera
-        // el timeout del Timer, sinó podria passar que ja tinguem els visors amb l'estudi carregat i el PopUp encara aparagués al centre de pantalla molestant
-        // a l'usuari
+        // If when the studio has been downloaded the PopUp has not yet moved down to the left, what we do is force them to move without waiting
+        // Timer timeout, otherwise it could happen that we already have the viewfinders with the studio loaded and the PopUp still appears in the center of the annoying screen
+        // to the user
         if (isMoveAnimationOnDelayPeriod())
         {
             cancelTriggeredAnimations();
@@ -85,10 +86,10 @@ void QPopUpRISRequestsScreen::retrieveDICOMFilesFromPACSJobFinished(PACSJobPoint
 
         if (m_pacsJobIDOfStudiesToRetrieve.contains(retrieveDICOMFilesFromPACSJob->getPACSJobID()))
         {
-            // Si no està a la llista de PACSJob per descarregar vol dir que és d'una altra petició de RIS que ha estat matxacada per l'actual
-            // Com que el QPopUpRisRequestScreen només segueix l'última petició del RIS les ignorem
+            // If it is not in the list of PACSJob to download it means that it is from another RIS request that has been crushed by the current one
+            // Because the QPopUpRisRequestScreen only follows the last RIS request we ignore them
             if (retrieveDICOMFilesFromPACSJob->getStatus() == PACSRequestStatus::RetrieveOk ||
-                retrieveDICOMFilesFromPACSJob->getStatus() == PACSRequestStatus::RetrieveSomeDICOMFilesFailed)
+                    retrieveDICOMFilesFromPACSJob->getStatus() == PACSRequestStatus::RetrieveSomeDICOMFilesFailed)
             {
                 m_numberOfStudiesRetrieved++;
             }
@@ -108,16 +109,17 @@ void QPopUpRISRequestsScreen::retrieveDICOMFilesFromPACSJobCancelled(PACSJobPoin
 
     if (retrieveDICOMFilesFromPACSJob.isNull())
     {
-        ERROR_LOG("El PACSJob que ha finalitzat no és un RetrieveDICOMFilesFromPACSJob");
+        ERROR_LOG("The completed PACSJob is not a RetrieveDICOMFilesFromPACSJob");
     }
     else
     {
-        // Si ha fallat o s'ha cancel·lat la descarrega l'estudi el treiem de la llista d'estudis a descarregar
+        /// If the download has failed or has been canceled, we will remove the study from the list of studies to be downloaded
         if (m_pacsJobIDOfStudiesToRetrieve.removeOne(retrieveDICOMFilesFromPACSJob->getPACSJobID()))
         {
             m_numberOfStudiesToRetrieve--;
-            // Si no està a la llista de PACSJob per descarregar vol dir que és d'una altra petició de RIS que ha estat matxacada per l'actual
-            // Com que el QPopUpRisRequestScreen només segueix l'última petició del RIS les ignorem
+            /// If it is not in the list of PACSJob to download it means that it is from another
+            ///RIS request that has been crushed by the current one
+            /// Because the QPopUpRisRequestScreen only follows the last RIS request we ignore them
             refreshScreenRetrieveStatus(retrieveDICOMFilesFromPACSJob->getStudyToRetrieveDICOMFiles());
         }
     }
@@ -160,7 +162,7 @@ void QPopUpRISRequestsScreen::showRetrieveFinished()
         {
             setOngoingOperationText(tr("No studies found.").arg(m_numberOfStudiesRetrieved));
         }
-        else 
+        else
         {
             setOngoingOperationText(tr("Unable to retrieve requested studies.").arg(ApplicationNameString));
         }
