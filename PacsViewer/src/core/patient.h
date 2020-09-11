9845f75d70024b53089fba98d0c4c1840966385c
zyq1569 <zyq1569@126.com>
2020-09-11 11:13:50 +0800

    try to  Splitter windows

--- PacsViewer/src/core/patient.h
+++ PacsViewer/src/core/patient.h
@@ -23,34 +23,34 @@
 namespace udg {
 
 /**
-    Classe que representa la unitat central de treball de l'aplicació: el Pacient.
-    Guardarà tota la informació relacionada amb aquests (Estudis, Sèries, etc.)
-  */
+Class that represents the central unit of work of the application: the Patient.
+It will save all the information related to these (Studies, Series, etc.)
+*/
 class Patient : public QObject {
-Q_OBJECT
+    Q_OBJECT
 public:
-    /// Enumeració dels graus de similitud
+    /// Enumeration of degrees of similarity
     enum PatientsSimilarity { SamePatients, DifferentPatients, SamePatientIDsDifferentPatientNames, SamePatientNamesDifferentPatientIDs };
 
     Patient(QObject *parent = 0);
 
-    /// Constructor per còpia, necessari pels operadors +, +=, -, -=
+    /// Copy constructor, required by operators +, + =, -, - =
     Patient(const Patient &patient, QObject *parent = 0);
     ~Patient();
 
-    /// Assigna/Obté el nom complet del pacient
+    /// Assign / Get the full name of the patient
     void setFullName(const QString &name);
     QString getFullName() const;
 
-    /// Assigna/Obté l'ID del pacient
+    /// Assign / Obtain the patient ID
     void setID(const QString &id);
     QString getID() const;
 
-    /// Assigna/Obté el ID de pacient a la BD d'Starviewer
+    /// Assign / Obtain the patient ID in the Starviewer database
     void setDatabaseID(qlonglong databaseID);
     qlonglong getDatabaseID() const;
 
-    /// Assigna/Obté data de naixement
+    /// Assign / Get date of birth
     void setBirthDate(int day, int month, int year);
     void setBirthDate(const QString &date);
     QString getBirthDateAsString();
@@ -59,39 +59,39 @@ public:
     int getMonthOfBirth();
     int getYearOfBirth();
 
-    /// Assigna/Obté sexe del pacient
+    /// Assign / Get sex from the patient
     void setSex(const QString &sex);
     QString getSex() const;
 
-    /// Afegeix un nou estudi. A l'estudi se li assigna com a "parentPatient" aquest Patient.
-    /// Retorna fals si existeix un estudi ja amb el mateix uid o l'uid és buit.
+    /// Add a new study. The study assigns this Patient as a "parentPatient".
+    /// Returns false if a study already exists with the same uid or the uid is empty.
     bool addStudy(Study *study);
 
-    /// Li treu al pacient l'estudi amb l'UID donat
+    /// Take the patient out of the study with the given UID
     void removeStudy(const QString &uid);
 
-    /// Obté l'estudi amb l'UID donat. Si no n'hi ha cap amb aquest uid retorna nul
+    /// Get the study with the given UID. If none with this uid returns null
     Study* getStudy(const QString &uid);
 
-    /// Retorna cert si aquest pacient té assignat l'estudi amb l'UID donat, fals altrament
+    /// Returns true if this patient is assigned the study with the given UID, otherwise false
     bool studyExists(const QString &uid);
 
-    /// Mètode per obtenir el nombre d'estudis del pacient
+    /// Method for obtaining the number of patient studies
     int getNumberOfStudies();
 
-    /// Mètode per obtenir la llista d'estudis del pacient. Per defecte ens la retorna ordenada amb els estudis més recents primer.
+    /// Method for obtaining the patient's list of studies. By default it returns
+    /// it to us in order with the most recent studies first.
     QList<Study*> getStudies(Study::StudySortType sortCriteria = Study::RecentStudiesFirst) const;
 
-    /// Ens retorna una llista amb els estudis del pacient que continguin la modalitat donada. La llista d'estudis no està en cap ordre concret.
+    /// Returns a list of the patient's studies that contain the given modality.
+    /// The list of studies is not in any specific order.
     QList<Study*> getStudiesByModality(const QString &modality);
-    
+
     /// Returns a string list with its studies modalities
     QStringList getModalities() const;
-    
-    /// Mètode ràpid per trobar si hi ha una series amb el uid donat. Retorna nul si aquesta no existeix
     Series* getSeries(const QString &uid);
 
-    /// Ens retorna una llista amb les sèries que estiguin seleccionades
+    /// Quick method to find if there is a series with the given uid. Returns null if it does not exist
     QList<Series*> getSelectedSeries();
     
     /// Returns a list of volumes that compose the patient.
@@ -100,29 +100,29 @@ public:
     /// Returns the number of volumes that compose the patient.
     int getNumberOfVolumes();
 
-    /// Ens diu si aquest arxiu pertany a alguna series del pacient
+    /// Tells us if this file belongs to any patient series
     bool hasFile(const QString &filename);
 
-    /// Operador d'assignació
+    /// Assignment operator
     Patient& operator =(const Patient &patient);
 
-    /// Operador de fusió de pacient. Amb aquest operador podrem unificar en un sol objecte Patient la informació de dos Patient
-    /// sempre que siguin identificats com a el mateix pacient. Això seria equivalent a un operador de Unió de conjunts
-    /// TODO aquí caldria rumiar-se si es pot forçar la fusió encara que siguin pacients diferents aparentment.
+    /// Patient fusion operator. With this operator we can unify in a single Patient object the information of two Patient
+    /// as long as they are identified as the same patient. This would be equivalent to a Union set operator
+    /// EVERYTHING here would have to be considered if fusion can be forced even if they are apparently different patients.
     Patient operator+(const Patient &patient);
     Patient operator+=(const Patient &patient);
 
-    /// Retorna si es considera que es pot identificar com al mateix pacient (a partir de l'ID i el nom).
-    /// No compara ni els estudis ni les sèries que conté, únicament la identificació de pacient.
+    /// Returns if it is considered that it can be identified as the same patient (from the ID and name).
+    /// It does not compare either the studies or the series it contains, only the patient identification.
     Patient::PatientsSimilarity compareTo(const Patient *patient) const;
 
-    /// Volca informació sobre el pacient en un string útil per tasques de debug
-    /// TODO Incorporar paràmetre "verbose" per poder escollir si fer un volcat complet o no
+    /// Dumps patient information into a useful string for debugging tasks
+    /// EVERYTHING Incorporate "verbose" parameter to be able to choose whether to make a complete dump or not
     QString toString() const;
 
-    /// Marca com a seleccionada la sèrie amb l'UID indicat
-    /// En cas que no existeixi tal UID, se selecciona la primera
-    /// sèrie del primer estudi
+    /// Mark the selected series with the indicated UID
+    /// In case no such UID exists, the first one is selected
+    /// series of the first study
     void setSelectedSeries(const QString &selectedSeriesUID);
 
 signals:
@@ -133,52 +133,53 @@ signals:
     void studyRemoved(Study*);
 
 private:
-    /// Donat un pacient, copia únicament la informació del pacient. No fa res respecte els estudis,series o imatges.
-    /// La informació es matxaca i no es fa cap mena de comprovació.
+    /// Given a patient, copy only the patient information. It does nothing about studies, series or images.
+    /// The information is crushed and no checks are made.
     void copyPatientInformation(const Patient *patient);
 
-    /// Inserta un estudi a la llista d'estudis ordenat per data. Els estudis més recents primer.
-    /// Pre: se presuposa que s'ha comprovat anteriorment que l'estudi no existeix a la llista
+    /// Inserts a study into the study list sorted by date. The most recent studies first.
+    /// Pre: It is assumed that it has been previously verified that the study does not exist in the list
     void insertStudy(Study *study);
 
-    /// Troba l'índex de l'estudi amb l'uid donat a la llista d'estudis
-    /// @param uid L'uid d'estudi que volem trobar
-    /// @return L'índex d'aquell estudi dins de la llista, -1 si no existeix l'estudi amb aquell uid.
+    /// Find the index of the study with the uid given in the list of studies
+    /// @param uid The study uid we want to find
+    /// @return The index of that study within the list, -1 if the study with that uid does not exist.
     int findStudyIndex(const QString &uid);
 
-    /// Crea un missatge de log per saber quins pacients estem fusionant
-    /// @param patient Les dades del pacient que es fusiona amb aquest
+    /// Create a log message to find out which patients we are merging
+    /// @param patient The data of the patient that merges with this one
     void patientFusionLogMessage(const Patient &patient);
 
-    // Returns the patient name without non-alphanumeric characters, with spaces at the beginning and at the end trimmed, and converted to uppercase.
+    /// Returns the patient name without non-alphanumeric characters,
+    /// with spaces at the beginning and at the end trimmed, and converted to uppercase.
     QString getCleanName() const;
 
 private:
-    /// Informació comuna de pacient per a totes les imatges que fan referència a aquest pacient. Apartat C.7.1.1 PS 3.3 DICOM.
-    /// Aquests són atributs del pacient necessaris per a interpretació diagnòstica de les imatges del pacient i són comunes per
-    /// a tots els estudis realitzats en el pacient. \TODO de moment no incloem cap atribut opcional
+    /// Common patient information for all images that refer to this patient. Section C.7.1.1 PS 3.3 DICOM.
+    /// These are patient attributes necessary for diagnostic interpretation of patient images and are common to
+    /// to all studies performed on the patient. \ EVERY time we do not include any optional attributes
 
-    /// Nom complet del pacient. (0010,0010) Tipus 2.
+    /// Full name of the patient. (0010,0010) Type 2.
     QString m_fullName;
 
-    /// Identificador primari donada al pacient per l'hospital. (0010,0020) Tipus 2.
+    /// Primary identifier given to the patient by the hospital. (0010,0020) Type 2.
     QString m_patientID;
 
-    /// ID de pacient a la bd d'Starviewer
+    /// Patient ID in Starviewer bd
     qlonglong m_databaseID;
 
-    /// Data de naixement. (0010,0030) Tipus 2.
+    /// Date of birth. (0010,0030) Type 2.
     QDate m_birthDate;
 
-    /// Sexe. (0010,0040) Tipus 2. M = male, F = female, O = other
+    /// Sex. (0010,0040) Type 2. M = male, F = female, O = other
     QString m_sex;
 
-    /// Indica si la identitat real del pacient s'ha tret dels atributs i de les dades. (0012,0062) Tipus 3. Considerem
-    /// aquest paràmetre per si és necessari a l'hora d'anonimatitzar. Això ens obligaria a fer servir els tags (0012,0063) i (0012,0064)
-    // TODO Aquest atribut encara no es farà servir, però és per tenir-ho en compte per si calgués.
+    /// Indicates whether the patient's actual identity has been removed from attributes and data. (0012,0062) Type 3. Consider
+    /// this parameter if necessary when anonymizing. This would force us to use the tags (0012,0063) and (0012,0064)
+    // ALL This attribute will not be used yet, but it is to be taken into account in case it is needed.
     bool m_identityIsRemoved;
 
-    /// Llista que conté els estudis del pacient ordenats per data
+    /// List containing patient studies sorted by date
     QList<Study*> m_studiesList;
 };
 
