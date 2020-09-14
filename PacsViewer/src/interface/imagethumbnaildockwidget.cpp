b8a498df2f07f55abbce469d0ef3c91132d0349f
zyq1569 <zyq1569@126.com>
2020-09-14 22:14:43 +0800

    try ???  add thumbnail

--- PacsViewer/src/interface/imagethumbnaildockwidget.cpp
+++ PacsViewer/src/interface/imagethumbnaildockwidget.cpp
@@ -1,14 +1,217 @@
 #include "imagethumbnaildockwidget.h"
 
+#include <QSize>
+#include <QListWidget>
+#include <QListWidgetItem>
+#include <QDir>
+#include <QVBoxLayout>
+#include <QPalette>
+const QSize IMAGE_SIZE(120,60);
+const QSize ITEM_SIZE(120,70);
+
 ImageThumbnailDockWidget::ImageThumbnailDockWidget(const QString &title, QWidget *parent,
                           Qt::WindowFlags flags ):QDockWidget(title, parent, flags)
 {
-
     m_background.setColor(QPalette::Background,Qt::black);
     setAutoFillBackground(true);
     setPalette(m_background);
+    m_ImagelistWidge = new QListWidget(this);
+    m_ImagelistWidge->setIconSize(IMAGE_SIZE);
+    m_ImagelistWidge->setResizeMode(QListView::Adjust);
+    m_ImagelistWidge->setViewMode(QListView::IconMode);
+    m_ImagelistWidge->setMovement(QListView::Static);
+    m_ImagelistWidge->setSelectionRectVisible(true);
+    m_ImagelistWidge->setSpacing(3);
+    QPalette palette;
+    palette.setColor(QPalette::Background, Qt::red);
+    m_ImagelistWidge->setPalette(palette);
+
+    m_mainlayout = new QHBoxLayout;
+    m_mainlayout->addWidget(m_ImagelistWidge);
+    m_mainlayout->setMargin(10);
+    setLayout(m_mainlayout);
+    m_ImagelistWidge->show();
+
 }
+
+
 ImageThumbnailDockWidget::~ImageThumbnailDockWidget()
 {
 
+}
+
+
+//QSize ImageThumbnailDockWidget::minimumSizeHint() const
+//{
+//    QDockWidget *dw = qobject_cast<QDockWidget*>(parentWidget());
+//    Q_ASSERT(dw);
+
+//    QSize result(m_pushpin.width(),m_pushpin.height());
+//    if (dw->features() & QDockWidget::DockWidgetVerticalTitleBar)
+//    {
+//        result.transpose();
+//    }
+
+//    return result;
+//}
+
+void ImageThumbnailDockWidget::paintEvent(QPaintEvent*)
+{
+//    QPainter painter(this);
+//    QRect rect = this->rect();
+
+//    QDockWidget *dw = qobject_cast<QDockWidget*>(parentWidget());
+//    Q_ASSERT(dw);
+
+//    if (dw->features() & QDockWidget::DockWidgetVerticalTitleBar) {
+//        QSize s = rect.size();
+//        s.transpose();
+//        rect.setSize(s);
+
+//        painter.translate(rect.left(), rect.top() + rect.width());
+//        painter.rotate(-90);
+//        painter.translate(-rect.left(), -rect.top());
+//    }
+
+//    painter.drawPixmap(rect.topLeft(), leftPm);
+//    painter.drawPixmap(rect.topRight() - QPoint(rightPm.width() - 1, 0), rightPm);
+//    QBrush brush(centerPm);
+//    painter.fillRect(rect.left() + leftPm.width(), rect.top(),
+//                        rect.width() - leftPm.width() - rightPm.width(),
+//                       centerPm.height(), centerPm);
+    QRect rect = this->rect();
+    m_ImagelistWidge->setGeometry(rect.left()+1,rect.top()+1,rect.right()-1,rect.bottom()-1);
+#ifdef QT_DEBUG
+    // PatientBrowserMenuExtendedItem
+    static bool init = false;
+    if (!init)
+    {
+        init = true;
+        QString path = "F:\\\log\\image";
+        QDir dir(path);
+        if (dir.exists())
+        {
+            dir.setFilter(QDir::Files | QDir::NoSymLinks);
+            QStringList filters;
+            filters<<"*.png"<<"*.jpg";
+            dir.setNameFilters(filters);
+            static QStringList m_imgList;
+            m_imgList = dir.entryList();
+            if (m_imgList.count()>0)
+            {
+                for (int i=0; i<m_imgList.count(); i++)
+                {
+                    QPixmap pixmap(path +"\\"+m_imgList.at(i));
+                    QListWidgetItem *item = new QListWidgetItem(QIcon(pixmap.scaled(IMAGE_SIZE)),m_imgList.at(i));
+                    item->setSizeHint(ITEM_SIZE);
+                    m_ImagelistWidge->insertItem(i,item);
+                }
+            }
+        }
+        //connect(m_ImagelistWidge, SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(slot_itemClicked(QListWidgetItem*)));
+    }
+#endif
+}
+
+void ImageThumbnailDockWidget::mouseReleaseEvent(QMouseEvent *event)
+{
+//    QPoint pos = event->pos();
+
+//    QRect rect = this->rect();
+
+//    QDockWidget *dw = qobject_cast<QDockWidget*>(parentWidget());
+//    Q_ASSERT(dw);
+
+//    if (dw->features() & QDockWidget::DockWidgetVerticalTitleBar) {
+//        QPoint p = pos;
+//        pos.setX(rect.left() + rect.bottom() - p.y());
+//        pos.setY(rect.top() + p.x() - rect.left());
+
+//        QSize s = rect.size();
+//        s.transpose();
+//        rect.setSize(s);
+//    }
+
+//    const int buttonRight = 7;
+//    const int buttonWidth = 20;
+//    int right = rect.right() - pos.x();
+//    int button = (right - buttonRight)/buttonWidth;
+//    switch (button) {
+//        case 0:
+//            event->accept();
+//            dw->close();
+//            break;
+//        case 1:
+//            event->accept();
+//            dw->setFloating(!dw->isFloating());
+//            break;
+//        case 2: {
+//            event->accept();
+//            QDockWidget::DockWidgetFeatures features = dw->features();
+//            if (features & QDockWidget::DockWidgetVerticalTitleBar)
+//                features &= ~QDockWidget::DockWidgetVerticalTitleBar;
+//            else
+//                features |= QDockWidget::DockWidgetVerticalTitleBar;
+//            dw->setFeatures(features);
+//            break;
+//        }
+//        default:
+//            event->ignore();
+//            break;
+//    }
+}
+
+void ImageThumbnailDockWidget::updateMask()
+{
+/*    QDockWidget *dw = qobject_cast<QDockWidget*>(parent());
+    Q_ASSERT(dw);
+
+    QRect rect = dw->rect();
+    QPixmap bitmap(dw->size());
+
+    {
+        QPainter painter(&bitmap);
+
+        // initialize to transparent
+        painter.fillRect(rect, Qt::color0);
+
+        QRect contents = rect;
+        contents.setTopLeft(geometry().bottomLeft());
+        contents.setRight(geometry().right());
+        contents.setBottom(contents.bottom()-y());
+        painter.fillRect(contents, Qt::color1);
+
+        // let's paint the titlebar
+        QRect titleRect = this->geometry();
+
+        if (dw->features() & QDockWidget::DockWidgetVerticalTitleBar) {
+            QSize s = rect.size();
+            s.transpose();
+            rect.setSize(s);
+
+            QSize s2 = size();
+            s2.transpose();
+            titleRect.setSize(s2);
+
+            painter.translate(rect.left(), rect.top() + rect.width());
+            painter.rotate(-90);
+            painter.translate(-rect.left(), -rect.top());
+        }
+
+        contents.setTopLeft(titleRect.bottomLeft());
+        contents.setRight(titleRect.right());
+        contents.setBottom(rect.bottom()-y());
+
+        QRect rect = titleRect;
+
+        painter.drawPixmap(rect.topLeft(), leftPm.mask());
+        painter.fillRect(rect.left() + leftPm.width(), rect.top(),
+            rect.width() - leftPm.width() - rightPm.width(),
+            centerPm.height(), Qt::color1);
+        painter.drawPixmap(rect.topRight() - QPoint(rightPm.width() - 1, 0), rightPm.mask());
+
+        painter.fillRect(contents, Qt::color1);
+    }
+
+    dw->setMask(bitmap);*/
 }
--- PacsViewer/src/interface/imagethumbnaildockwidget.h
+++ PacsViewer/src/interface/imagethumbnaildockwidget.h
@@ -1,6 +1,9 @@
 #ifndef IMAGETHUMBNAILDOCKWIDGET_H
 #define IMAGETHUMBNAILDOCKWIDGET_H
 
+class QListWidget;
+class QVBoxLayout;
+class QHBoxLayout;
 #include <QDockWidget>
 
 class ImageThumbnailDockWidget : public QDockWidget
@@ -10,8 +13,27 @@ public:
     ImageThumbnailDockWidget(const QString &title, QWidget *parent = nullptr,
                              Qt::WindowFlags flags = Qt::WindowFlags());
     ~ImageThumbnailDockWidget();
+
+public:
+    //    QSize sizeHint() const override
+    //    {
+    //        return minimumSizeHint();
+    //    }
+    //    QSize minimumSizeHint() const override;
+
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+public slots:
+    void updateMask();
+
 private:
     QPalette m_background;
+    const QPixmap m_pushpin;
+    QListWidget *m_ImagelistWidge;
+    QHBoxLayout *m_mainlayout;
 };
 
 #endif // IMAGETHUMBNAILDOCKWIDGET_H
