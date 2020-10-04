c16f6ea40850d2a2caa28154c869818f28a89b12
zyq1569 <zyq1569@126.com>
2020-10-04 22:16:15 +0800

    modify  comments

--- PacsViewer/src/inputoutput/qpopuprisrequestsscreen.h
+++ PacsViewer/src/inputoutput/qpopuprisrequestsscreen.h
@@ -26,10 +26,10 @@ class Patient;
 class Study;
 
 /**
-    Classe que mostra un PopUp indicant que s'ha rebut una petició del RIS per descarregar un estudi
+    Class showing a PopUp indicating that a request has been received from the RIS to download a study
   */
 class QPopUpRISRequestsScreen : public QNotificationPopup {
-Q_OBJECT
+    Q_OBJECT
 public:
     /// Constructor
     QPopUpRISRequestsScreen(QWidget *parent = 0);
@@ -37,36 +37,39 @@ public:
     /// Destructor
     ~QPopUpRISRequestsScreen();
 
-    /// S'indica que s'està cercant als PACS els estudis amb un determinat accessionnumber
+    ///It is indicated that studies with a certain accession number are being searched in PACS
     void queryStudiesByAccessionNumberStarted();
 
-    /// Afegim un estudi per descarregar que s'ha demanat a través del rIS
+    /// We added a downloadable study that was requested through the rIS
     void addStudyToRetrieveFromPACSByAccessionNumber(PACSJobPointer retrieveDICOMFilesFromPACSJob);
 
-    /// Afegim un estudi que s'ha obtingut de la base de dades del PACS demana't en una petició del RIS
-    //HACK: Com ara mateix nom tenim cap manera de fer el seguiment de les peticions d'estudis a la base de dades, afegim aquest mètode que mostarà
-    //aquest estudi ja com a descarregat i el sumarà al total d'estudis
+    /// We add a study that has been obtained from the PACS database ask yourself in a request of the RIS
+    /// HACK: As of right now we have no way of tracking the
+    /// study requests to the database, we add this method which will show
+    /// this study is already downloaded and will add it to the total studies
     void addStudyRetrievedFromDatabaseByAccessionNumber(Study *study);
 
     /// Mostra en el PopUp que no s'ha trobat cap estudi que compleixi els criteris de cerca
     void showNotStudiesFoundMessage();
 
-    /// S'especifica els segons que ha de tardar el PopUp a amagar-se després que s'hagin acabat de descarregar totes els estudis
-    /// Sinó s'especifica per defecte són 5000 ms
+    /// Specifies the seconds that the PopUp should take to hide
+    /// after all studies have been downloaded
+    /// Otherwise specified is 5000 ms
     void setTimeOutToHidePopUpAfterStudiesHaveBeenRetrieved(int timeOutms);
 
 private slots:
-    /// S'indica que la descàrrega dels estudis degut a una petició del RIS ha finalitzat
+    /// It is indicated that the download of the studies due to a request from the RIS has ended
     void retrieveDICOMFilesFromPACSJobFinished(PACSJobPointer pacsJob);
 
-    /// S'indica que ha fallat o s'ha cancel·lat la descàrrega dels estudi demanat pel RIS
+    /// It is stated that the download of the studies requested by the RIS has failed or has been canceled
     void retrieveDICOMFilesFromPACSJobCancelled(PACSJobPointer pacsJob);
 
 private:
-    /// Actualitza per pantalla l'estat de la descàrrege
+    ///Updates the download status on the screen
     void refreshScreenRetrieveStatus(Study *study);
 
-    /// Indica que la descàrrega ha finalitzat, indica el número d'estudis descarregats i el cap de 5 segons amaga el PopUp
+    /// Indicates that the download is complete, indicates
+    /// number of studies downloaded and after 5 seconds hides the PopUp
     void showRetrieveFinished();
 
 private:
@@ -74,7 +77,7 @@ private:
     int m_numberOfStudiesRetrieved;
     int m_numberOfStudiesFailedToRetrieve;
 
-    /// Guarda el número d'estudis que s'han de descarregar
+    ///Save the number of studies to be downloaded
     int m_numberOfStudiesToRetrieve;
 };
 
