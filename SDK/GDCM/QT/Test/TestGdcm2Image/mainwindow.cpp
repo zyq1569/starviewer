f98eb3013d33fc6de45a6e66ede649cd76cd9e1a
zyq1569 <zyq1569@126.com>
2023-05-18 11:16:50 +0800

    add Test

--- SDK/GDCM/QT/Test/TestGdcm2Image/mainwindow.cpp
+++ SDK/GDCM/QT/Test/TestGdcm2Image/mainwindow.cpp
@@ -50,6 +50,7 @@ MainWindow::~MainWindow()
 //Implicit VR Little Endian:
 //Default Transfer Syntax for DICOM               1.2.840.10008.1.2
 //Explicit VR Little Endian                       1.2.840.10008.1.2.1
+//Deflated Explicit VR Little Endian              1.2.840.10008.1.2.1.99
 //Explicit VR Big Endian                          1.2.840.10008.1.2.2
 
 //无损压缩：
@@ -128,8 +129,57 @@ MainWindow::~MainWindow()
 
 
 ///
-
+//未压缩：
+//Transfer Syntax UID Transfer Syntax name
+//Implicit VR Endian: Default Transfer Syntax for DICOM(默认，使用广)          1.2.840.10008.1.2
+// Explicit VR Little Endian                                                   1.2.840.10008.1.2.1
+//Deflated Explicit VR Little Endian                                           1.2.840.10008.1.2.1.99
+// Explicit VR Big Endian                                                      1.2.840.10008.1.2.2
+//压缩---JPEG和JPEG2000（有损和无损）
+//有损：值中带Lossy
+//Transfer Syntax UID Transfer Syntax name
+//1.2.840.10008.1.2.4.50 JPEG Baseline (Process 1):
+//Default Transfer Syntax for Lossy JPEG 8-bit Image Compression
+//1.2.840.10008.1.2.4.51 JPEG Baseline (Processes 2 & 4):
+//Default Transfer Syntax for Lossy JPEG 12-bit Image Compression
+//(Process 4 only)
+//1.2.840.10008.1.2.4.52 JPEG Extended (Processes 3 & 5)
+//1.2.840.10008.1.2.4.53 JPEG Spectral Selection, Nonhierarchical (Processes 6 & 8)
+//1.2.840.10008.1.2.4.54 JPEG Spectral Selection, Nonhierarchical (Processes 7 & 9)
+//1.2.840.10008.1.2.4.55 JPEG Full Progression, Nonhierarchical (Processes 10 & 12)
+//1.2.840.10008.1.2.4.56 JPEG Full Progression, Nonhierarchical (Processes 11 & 13)
+//1.2.840.10008.1.2.4.57 JPEG Lossless, Nonhierarchical (Processes 14)
+//1.2.840.10008.1.2.4.58 JPEG Lossless, Nonhierarchical (Processes 15)
+//1.2.840.10008.1.2.4.59 JPEG Extended, Hierarchical (Processes 16 & 18)
+//1.2.840.10008.1.2.4.60 JPEG Extended, Hierarchical (Processes 17 & 19)
+//1.2.840.10008.1.2.4.61 JPEG Spectral Selection, Hierarchical (Processes 20 & 22)
+//1.2.840.10008.1.2.4.62 JPEG Spectral Selection, Hierarchical (Processes 21 & 23)
+//1.2.840.10008.1.2.4.63 JPEG Full Progression, Hierarchical (Processes 24 & 26)
+//1.2.840.10008.1.2.4.64 JPEG Full Progression, Hierarchical (Processes 25 & 27)
+//1.2.840.10008.1.2.4.65 JPEG Lossless, Nonhierarchical (Process 28)
+//1.2.840.10008.1.2.4.66 JPEG Lossless, Nonhierarchical (Process 29)
+//1.2.840.10008.1.2.4.70 JPEG Lossless, Nonhierarchical, First- Order Prediction
+//(Processes 14 [Selection Value 1]):
+//Default Transfer Syntax for Lossless JPEG Image Compression
+//1.2.840.10008.1.2.4.80 JPEG-LS Lossless Image Compression
+//DICOM Transfer Syntaxes
+//1.2.840.10008.1.2.4.81 JPEG-LS Lossy (Near- Lossless) Image Compression
+//1.2.840.10008.1.2.4.90 JPEG 2000 Image Compression (Lossless Only)
+//1.2.840.10008.1.2.4.91 JPEG 2000 Image Compression
+//1.2.840.10008.1.2.4.92 JPEG 2000 Part 2 Multicomponent Image Compression (Lossless Only)
+//1.2.840.10008.1.2.4.93 JPEG 2000 Part 2 Multicomponent Image Compression
+//Transfer Syntax UID Transfer Syntax name
+//1.2.840.10008.1.2.4.94 JPIP Referenced
+//1.2.840.10008.1.2.4.95 JPIP Referenced Deflate
+//1.2.840.10008.1.2.5 RLE Lossless
+//1.2.840.10008.1.2.6.1 RFC 2557 MIME Encapsulation
+//Transfer Syntax UID Transfer Syntax name
+//1.2.840.10008.1.2.4.100 MPEG2 Main Profile Main Level
+//1.2.840.10008.1.2.4.102 MPEG-4 AVC/H.264 High Profile / Level 4.1
+//1.2.840.10008.1.2.4.103 MPEG-4 AVC/H.264 BD-compatible High Profile / Level 4.1
 ///dcmdjpeg.cc dcmcjpeg.cc
+///
+/// https://dicomlibrary.com/dicom/transfer-syntax/
 const QString PreviewNotAvailableText(QObject::tr("Preview image not available"));
 
 QImage makeEmptyThumbnailWithCustomText(const QString &text, int resolution = 96)
@@ -468,7 +518,7 @@ void MainWindow::on_decoder_clicked()
             OFCondition error = EC_Normal;
 
             E_TransferSyntax opt_ixfer = EXS_LittleEndianImplicit;
-            switch(m_TransferSyntax-1)
+            switch(m_TransferSyntax)
             {
             case 1:
                 opt_ixfer = EXS_LittleEndianImplicit;
