61022163d796156b098bc3ba9caa1d54c3669cd5
zyq1569 <zyq1569@126.com>
2025-01-02 16:03:40 +0800

    fine-tune

--- PacsViewer/src/main/extensions.h
+++ PacsViewer/src/main/extensions.h
@@ -2,7 +2,7 @@
 #define EXTENSIONS_H
 //#include "../extensions/main/mpr/mprextensionmediator.h"
 #include "../extensions/main/mpr3d/mpr3dextensionmediator.h"
-//#include "../extensions/main/dicomprint/dicomprintextensionmediator.h"
+#include "../extensions/main/dicomprint/dicomprintextensionmediator.h"
 #include "../extensions/main/q2dviewer/q2dviewerextensionmediator.h"
 #include "../extensions/main/q3dviewer/q3dviewerextensionmediator.h"
 //#include "../extensions/main/pdf/pdfextensionmediator.h"
@@ -16,7 +16,7 @@
 void initExtensionsResources()
 {
 	//Q_INIT_RESOURCE(mpr);
-	//Q_INIT_RESOURCE(dicomprint);
+	Q_INIT_RESOURCE(dicomprint);
 	Q_INIT_RESOURCE(q2dviewer);
 	//Q_INIT_RESOURCE(mpr3D);
 	Q_INIT_RESOURCE(q3dviewer);
